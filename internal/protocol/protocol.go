9f0e14df13d790db745ce480dfcd7c9dcc18b3fc
Jerry <jencisollallahui@gmail.com>
2026-06-04 00:39:21 -0500

    feat protocol.go

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -2,19 +2,23 @@ package protocol
 
 import "encoding/json"
 
+type CmdType string
+
 const (
-	CmdAuth = "AUTH" // Para registrar el nickname al conectar
-	CmdMsg  = "MSG"  //Para mensajes de chat normales
-	CmdJoin = "JOIN" //Para unirse a una sala especifica
+	CmdAuth CmdType = "AUTH" // Para registrar el nickname al conectar
+	CmdMsg          = "MSG"  //Para mensajes de chat normales
+	CmdJoin         = "JOIN" //Para unirse a una sala especifica
 )
 
 type Message struct {
-	Type    string `json:"type"`    //¿Que acción es? (ej.MSG)
-	From    string `json:"from"`    //¿Quien lo envía? (tu nickname)
-	Room    string `json:"room"`    // ¿A que sala va? (ej general)
-	Payload string `json:"payload"` //¿El contenido real del mensaje
+	Type      CmdType `json:"type"`                //¿Que acción es? (ej.MSG)
+	From      string  `json:"from,omitempty"`      //¿Quien lo envía? (tu nickname)
+	Room      string  `json:"room,omitempty"`      // ¿A que sala va? (ej general)
+	Payload   string  `json:"payload,omitempty"`   //¿El contenido real del mensaje
+	Timestamp int64   `json:"timestamp,omitempty"` //Con timestamp, el cliente puede reordenar y mostrarlos correctamente
 }
 
-func (m *Message) ToJson() ([]byte, error) {
+func (m *Message) ToJSON() ([]byte, error) {
 	return json.Marshal(m)
 }
+func NewMessage() *Message {}
